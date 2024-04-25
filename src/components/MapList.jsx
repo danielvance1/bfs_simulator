47bb7d503a8428aeb6d78f83f11b0762c1abae7d
danielvance1 <danielvance2019@gmail.com>
2024-04-24 19:45:51 -0600

    updates to ui

---
+++ src/components/MapList.jsx
@@ -0,0 +1,7 @@
+import classes from "./MapList.module.css"
+
+function MapList(){
+    return <div className={classes.mapList}></div>
+}
+
+export default MapList
---
+++ src/components/NewMap.jsx
@@ -0,0 +1,44 @@
+import classes from "./NewMap.module.css"
+
+import MapList from "./MapList"
+
+import { FaCloudUploadAlt } from "react-icons/fa";
+
+import { useState } from "react"
+
+function NewMap(props){
+    const [mapName, setMapName] = useState("")
+
+    function mapNameChangeHandler(event){
+        setMapName(event.target.value)
+    }
+
+    return (
+        <div className={classes.newMap}>
+            <input className={classes.mapName} type="text" placeholder="Name your map" onChange={mapNameChangeHandler}/>
+            
+            {!props.startGood &&
+                <p className={classes.error}>Start cell required</p>
+            }
+
+            {!props.endGood &&
+                <p className={classes.error}>End cell required</p>
+            }
+
+            {mapName.length===0 &&
+                <p className={classes.error}>Map name required</p>
+            }
+
+            <button className={(!props.endGood || !props.startGood || mapName.length===0) ? classes.disabledSaveMap : classes.saveMap} disabled={!props.endGood || !props.startGood || mapName.length===0}>
+                <span>Upload your map <FaCloudUploadAlt /></span>
+            </button>
+        </div>
+    )
+}
+
+export default NewMap
+
+// <button className={classes.invalidMap}>
+//                     <span>Save your map!</span>
+//                     <p></p>
+//                 </button>
